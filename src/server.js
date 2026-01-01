530cc79eb54876b2c43f3bcbcfb0f8409bd7d89f
Abed001 <abed.alroufe@gmail.com>
2026-01-01 16:20:19 +0200

    resoloving cors bug

--- src/server.js
+++ src/server.js
@@ -13,13 +13,7 @@ const githubRoutes = require('./routes/github'); // ← Add this
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-};
-
-app.use(cors(corsOptions));
+const allowedOrigins = ["https://devboard-frontend-mu.vercel.app", "http://localhost:5173"]
 app.use(express.json());
 
 // Test route
