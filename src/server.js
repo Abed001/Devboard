18ddbaa810c91b090d94e873f0021a6ed7925282
Abed001 <abed.alroufe@gmail.com>
2026-01-04 17:53:22 +0200

    Fix build for deployment

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const githubRoutes = require('./routes/github'); // ← Add this
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+/*
 const allowedOrigins = [
   'https://devboard-client-zqwd.onrender.com',
   "http://localhost:5173"
@@ -34,8 +35,8 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicitly allow common methods
   allowedHeaders: ['Content-Type', 'Authorization'] // Explicitly allow common headers
 };
-
-app.use(cors(corsOptions));
+*/
+app.use(cors());
 // --- ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ---
 
 app.use(express.json());
